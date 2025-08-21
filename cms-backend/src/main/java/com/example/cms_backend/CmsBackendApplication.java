06c1a0cf1fc1068765cea988a1fd367803549990
Pongsaphak <39190909+pongsaphakmw@users.noreply.github.com>
2025-08-22 00:28:48 +0700

    feat: add Dockerfile and base image for backend, update pom.xml for web starter, and implement health check endpoint

--- cms-backend/src/main/java/com/example/cms_backend/CmsBackendApplication.java
+++ cms-backend/src/main/java/com/example/cms_backend/CmsBackendApplication.java
@@ -2,12 +2,18 @@ package com.example.cms_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CmsBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CmsBackendApplication.class, args);
 	}
 
+	@GetMapping("/health")
+	public String healthCheck() {
+		return "OK";
+	}
 }
